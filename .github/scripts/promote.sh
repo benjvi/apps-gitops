c4a41088db01eb2a96372be02a4faa351aebbdea
Ben Vickers <bvickers@vmware.com>
2021-03-31 15:30:52 +0200

    fix rsync syntax

--- .github/scripts/promote.sh
+++ .github/scripts/promote.sh
@@ -5,7 +5,7 @@ SRCDIR="$1"
 TGTDIR="$2"
 
 mkdir -p "$TGTDIR"
-rsync -a --exclude 'prify.yml' "$SRCDIR" "$TGTDIR"
+rsync -a --exclude 'prify.yml' "$SRCDIR/" "$TGTDIR/"
 git --no-pager diff --summary
 
 cd "$TGTDIR"
